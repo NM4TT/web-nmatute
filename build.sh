e8a4d3dc483519b712806c9487d3d9076cd15083
NM4TT <manuelnmatute@gmail.com>
2025-01-25 19:49:07 -0500

    update some configs

--- build.sh
+++ build.sh
@@ -4,15 +4,17 @@
 set -e
 
 DEBUG_MODE=false
+
 if [[ "$1" == "--debug" || "$1" == "-d" ]]; then
   DEBUG_MODE=true
   echo "Debug mode enabled: Using docker-compose.dev.yaml"
 fi
 
+echo "$ROOT_PASS" | sudo -S chmod -R 750 .
 docker container prune -f && docker image prune -a -f
 
 if [ "$DEBUG_MODE" = true ]; then
-  docker-compose -f "./docker-compose.yaml" -f "./docker-compose.dev.yaml" up -d --build --force-recreate
+  docker compose -f "./docker-compose.yaml" -f "./docker-compose.dev.yaml" up -d --build --force-recreate
 else
-  docker-compose -f "./docker-compose.yaml" up -d --build --force-recreate
+  docker compose -f "./docker-compose.yaml" up -d --build --force-recreate
 fi
